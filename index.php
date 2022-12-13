4204ff95b56678d70091fd4873727293c10c9913
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-12-13 16:02:39 +0100

    Aggiunta stampa paragrafo e lunghezza intera

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
+<?php
+$text="Correva l&#39; anno 1994 quando Rasmus Lerdorf, giovane programmatore groenlandese, ipotizzò un nuovo linguaggio di programmazione per rendere le pagine web più dinamiche: l&#39 8 giugno 1995 presentò la prima versione di PHP.";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP Badwords</title>
 </head>
 <body>
+    <!-- stampa paragrafo e lunghezza per intero -->
+    <p><?php echo $text ?></p>
+    <h4><?php echo strlen($text) ?></h4>
     
 </body>
 </html>
